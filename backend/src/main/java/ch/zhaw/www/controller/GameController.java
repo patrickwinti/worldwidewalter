d4ea71ee733f935853781f53de626f9824adc57d
schwipa2 <schwipa2@students.zhaw.ch>
2023-03-14 14:53:49 +0100

    [WWW-46] - allow CrossOrigin requests

--- backend/src/main/java/ch/zhaw/www/controller/GameController.java
+++ backend/src/main/java/ch/zhaw/www/controller/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Controller for "games" resource.
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RestController
 @Validated
+@CrossOrigin(origins = "http://localhost:4200")
 public class GameController {
     private final GameService gameService;
 

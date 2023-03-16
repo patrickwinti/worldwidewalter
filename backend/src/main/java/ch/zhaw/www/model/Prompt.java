a9663e23efeedac2cbf463866076e98619c47629
Ricardo Duarte <duartric@students.zhaw.ch>
2023-03-16 19:41:52 +0100

    [WWW-28] - Deleted Placeholder class - Attributes and basic methods added to Prompt class

--- backend/src/main/java/ch/zhaw/www/model/Prompt.java
+++ backend/src/main/java/ch/zhaw/www/model/Prompt.java
@@ -1,4 +1,28 @@
 package ch.zhaw.www.model;
 
+/**
+ * Prompt Class stores a sentence with a statements that has one or more "placeholder" words to be completed
+ * by the players' proposition.
+ * The prompt class stores the total number of placeholder in the statement.
+ */
+
 public class Prompt {
+
+    private int totalPlaceholders;
+    private String  statement;
+
+    public Prompt (int totalPlaceholders, String statement) {
+        this.totalPlaceholders = totalPlaceholders;
+        this.statement = statement;
+    }
+
+    public int getTotalPlaceholders () {
+        return totalPlaceholders;
+    }
+
+    public String getStatement () {
+        return statement;
+    }
+
+
 }

b32c3bd7d33133efed7cc10adc1364778ecdcb0c
Ricardo Duarte <duartric@students.zhaw.ch>
2023-03-17 20:53:09 +0100

    [WWW-28] Adding Test templates for FileReader Classes and Prompt Class

---
+++ backend/src/test/java/ch/zhaw/www/PromptTest.java
@@ -0,0 +1,16 @@
+package ch.zhaw.www;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PromptTest {
+
+    @Test
+    void getTotalPlaceholders() {
+    }
+
+    @Test
+    void getStatement() {
+    }
+}
---
+++ backend/src/test/java/ch/zhaw/www/TXTFileReaderTest.java
@@ -0,0 +1,12 @@
+package ch.zhaw.www;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TXTFileReaderTest {
+
+    @Test
+    void readFile() {
+    }
+}

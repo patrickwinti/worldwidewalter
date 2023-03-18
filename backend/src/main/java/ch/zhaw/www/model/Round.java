5c2834047952e1d53828d229a1c9daff4ae1898a
Felix <opplifel@students.zhaw.ch>
2023-03-18 15:02:40 +0100

    updated fields in Round

--- backend/src/main/java/ch/zhaw/www/model/Round.java
+++ backend/src/main/java/ch/zhaw/www/model/Round.java
@@ -4,14 +4,21 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Map;
+
 /**
  * Model class with round information
  */
 @AllArgsConstructor
 @Data
 public class Round {
+    Map<Player, String> propositions;
+    Map<Player, String> selections;
+
     @NotNull
-    String id;
+    final String id;
     @NotNull
     String prompt;
+
+
 }

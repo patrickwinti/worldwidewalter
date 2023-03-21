ef57a6d907ecff4cc5be28c4ec04ab3781de2558
vaquecar <vaquecar@students.zhaw.ch>
2023-03-21 07:25:52 +0100

    [WWW-64] game repository in service

--- backend/src/main/java/ch/zhaw/www/model/Game.java
+++ backend/src/main/java/ch/zhaw/www/model/Game.java
@@ -2,6 +2,7 @@ package ch.zhaw.www.model;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.keyvalue.annotation.KeySpace;
 
@@ -10,10 +11,11 @@ import org.springframework.data.keyvalue.annotation.KeySpace;
  * Model class with all the current state
  * of a game.
  */
+@RequiredArgsConstructor
 @Getter
 @KeySpace("running_games")
 public class Game {
     @Id
     @NotNull
-    String id;
+    private final String id;
 }

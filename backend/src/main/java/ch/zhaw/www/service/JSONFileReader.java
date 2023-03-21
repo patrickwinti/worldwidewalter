237c9775817e68841f35b4d395cdb910140102ef
Ricardo Duarte <duartric@students.zhaw.ch>
2023-03-21 19:03:01 +0100

    [WWW-28] - changing JSONFileReader

--- backend/src/main/java/ch/zhaw/www/service/JSONFileReader.java
+++ backend/src/main/java/ch/zhaw/www/service/JSONFileReader.java
@@ -1,13 +1,15 @@
 package ch.zhaw.www.service;
-
 import ch.zhaw.www.model.Prompt;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.ArrayList;
+import java.nio.file.Files;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 /**
  * Public class to read Files of JSON type.
  * Class implements FileReader interface.
@@ -19,30 +21,34 @@ public class JSONFileReader implements FileReader {
      * @param file JSON file input
      * @return List with parsed prompts
      */
-
-    public List<Prompt> readFile (File file) {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<Prompt> prompts = new ArrayList<>();
-// todo add expected format checker
-        try {
-            JsonNode jsonNode = objectMapper.readTree(file);
-
-            JsonNode cardsNode = jsonNode.get("cards");
-
-            // Loop over the child nodes of the "cards" field
-            for (JsonNode cardNode : cardsNode) {
-
-                // Access the fields of each card using the get() method
-                prompts.add(new Prompt(cardNode.get("1").asText()));
-                prompts.add(new Prompt(cardNode.get("2").asText()));
-                prompts.add(new Prompt(cardNode.get("3").asText()));
-            }
-
+    public List<Prompt> readFile(File file) {
+        try (BufferedReader fileReader = Files.newBufferedReader(file.toPath())) {
+            ObjectMapper mapper = new ObjectMapper();
+            Deck deck = mapper.readValue(fileReader, Deck.class);
+            return deck.cards.values().stream()
+                    .flatMap(Card::toStream)
+                    .filter(Objects::nonNull)
+                    .map(Prompt::new)
+                    .toList();
         } catch (Exception e) {
             throw new FileReaderError.WrongFileFormatError();
         }
-        return prompts;
+    }
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private static class Deck {
+        @JsonProperty("cards")
+        private Map<String, Card> cards;
+    }
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private static class Card {
+        @JsonProperty("1")
+        private String first;
+        @JsonProperty("2")
+        private String second;
+        @JsonProperty("3")
+        private String three;
+        Stream<String> toStream() {
+            return Stream.of(first, second, three);
+        }
     }
 }
-

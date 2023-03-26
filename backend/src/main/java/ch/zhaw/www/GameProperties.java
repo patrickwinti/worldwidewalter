d300a3bb27be0bfc11c5274fea3cc35a5b1f7500
vaquecar <vaquecar@students.zhaw.ch>
2023-03-26 12:33:05 +0200

    [WWW-72] adds javadoc to properties

--- backend/src/main/java/ch/zhaw/www/GameProperties.java
+++ backend/src/main/java/ch/zhaw/www/GameProperties.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * Container for environment properties
+ */
 @Getter
 @Configuration
 @PropertySource("classpath:config.properties")

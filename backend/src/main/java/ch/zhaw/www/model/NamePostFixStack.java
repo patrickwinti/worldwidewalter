928adb66dcff49ab47fae4039b69d9ed9f9373f7
Felix <opplifel@students.zhaw.ch>
2023-03-27 13:06:08 +0200

    added javadoc to NamePostFixStack.java

--- backend/src/main/java/ch/zhaw/www/model/NamePostFixStack.java
+++ backend/src/main/java/ch/zhaw/www/model/NamePostFixStack.java
@@ -2,7 +2,10 @@ package ch.zhaw.www.model;
 
 
 import java.util.Stack;
-
+/**
+ * This class represents a stack of name postfixes.
+ * The postfixes are used to create unique names for players.
+ */
 public class NamePostFixStack {
     private final Stack<String> namePostFix = new Stack<>();
 

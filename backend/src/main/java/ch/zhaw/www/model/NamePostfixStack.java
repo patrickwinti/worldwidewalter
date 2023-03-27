3fb2cc5e1f45bda24992d763c75763dfd1393608
Felix <opplifel@students.zhaw.ch>
2023-03-27 14:10:30 +0200

    further updated javadoc in NamePostfixStack.java (grammar)

--- backend/src/main/java/ch/zhaw/www/model/NamePostfixStack.java
+++ backend/src/main/java/ch/zhaw/www/model/NamePostfixStack.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * This class contains a stack of name postfixes.
  * The postfixes are used to create unique names for players.
  * Note that this stack only contains 12 postfixes, so if more than 12 players of the same name join the game,
- * (which currently is not possible) the Stack will be empty and will throw a StackIsEmptyException.
+ * (which currently is not possible) the Stack will be empty and will throw an EmptyStackException.
  */
 public class NamePostfixStack {
     private final Stack<String> postFixes = new Stack<>();

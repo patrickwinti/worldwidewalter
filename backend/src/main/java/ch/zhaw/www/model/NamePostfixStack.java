43cef7aefe85f6cac35a3fb4502ef39e4110748e
Felix <opplifel@students.zhaw.ch>
2023-03-27 13:43:34 +0200

    updated javadoc in NamePostfixStack.java

--- backend/src/main/java/ch/zhaw/www/model/NamePostfixStack.java
+++ backend/src/main/java/ch/zhaw/www/model/NamePostfixStack.java
@@ -3,7 +3,7 @@ package ch.zhaw.www.model;
 
 import java.util.Stack;
 /**
- * This class represents a stack of name postfixes.
+ * This class contains a stack of name postfixes.
  * The postfixes are used to create unique names for players.
  */
 public class NamePostfixStack {

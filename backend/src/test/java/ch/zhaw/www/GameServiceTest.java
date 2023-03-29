cbd50d06dfd2984b3c2654b2841c0350b03c8ee4
Ricardo Duarte <duartric@students.zhaw.ch>
2023-03-29 20:13:16 +0200

    [WWW-70] Changes: - tried another approach for testSubmitProposition

--- backend/src/test/java/ch/zhaw/www/GameServiceTest.java
+++ backend/src/test/java/ch/zhaw/www/GameServiceTest.java
@@ -29,52 +29,54 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class GameServiceTest {
-    
+
     private static final String GAME_ID = "GAME ID";
     private static final String UNKNOWN_PLAYER_ID = "Unknown Player";
     private static final int ROUND_DURATION = 4;
-    
+
+    private static final Prompt PROMPT = new Prompt("Hello my name is WALTER", 1);
+
     @Autowired
     private GameService gameService;
     @MockBean
     private GameEntityService gameEntityService;
-    
+
     @Test
     void testAddGameSavesItToRepository() {
         var game = gameService.createGame();
         verify(gameEntityService).saveNewGame(game);
         assertNotNull(game.getId());
     }
-    
+
     @Test
     void testGetGameReadsFromRepository_Found() {
         var expectedGame = mockGameInRepository();
         var actualGame = gameService.getGame(expectedGame.getId());
         assertEquals(expectedGame, actualGame);
     }
-    
+
     @Test
     void testGetGameReadsFromRepository_NotFound() {
         var gameId = "jibberish";
         mockGameNotFoundInRepository(gameId);
         assertThrows(GameError.NotFoundException.class, () -> gameService.getGame(gameId));
     }
-    
+
     private static Round getRound() {
         return new Round("1", new Prompt("WALTER!", 1), ROUND_DURATION, 1);
     }
-    
+
     private static Player getRandomPlayer(Game game) {
         return game.getWaitingRoom().values().iterator().next();
     }
-    
+
     @Test
     void testGetRound_IllegalState() {
         var game = mockGameInRepository();
-        
+
         Round round = getRound();
         game.addRound(round);
-        
+
         Player player1 = addWaitingRoomPlayer(game);
         assertThrows(RoundError.IllegalStateException.class, () -> gameService.getRound(GAME_ID, player1.getId()));
         Player player2 = addWaitingRoomPlayer(game);
@@ -83,7 +85,7 @@ class GameServiceTest {
         assertThrows(RoundError.IllegalStateException.class, () -> gameService.getRound(GAME_ID, player1.getId()));
         Player player4 = addWaitingRoomPlayer(game);
         assertThrows(RoundError.IllegalStateException.class, () -> gameService.getRound(GAME_ID, player1.getId()));
-        
+
         InstantWrapper.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
         round.setSphinx(getRandomPlayer(game));
         addActivePlayer(game, player1);
@@ -91,12 +93,12 @@ class GameServiceTest {
         addActivePlayer(game, player3);
         addActivePlayer(game, player4);
         assertEquals(round, gameService.getRound(GAME_ID, player1.getId()));
-        
+
         InstantWrapper.clock = Clock.offset(InstantWrapper.clock, Duration.of(ROUND_DURATION, ChronoUnit.MINUTES));
-        
+
         assertThrows(RoundError.IllegalStateException.class, () -> gameService.getRound(GAME_ID, player1.getId()));
     }
-    
+
     @Test
     void testGetRound_UnknownPlayer() {
         var game = mockGameInRepository();
@@ -107,17 +109,17 @@ class GameServiceTest {
         addWaitingRoomPlayer(game);
         addWaitingRoomPlayer(game);
         round.setSphinx(getRandomPlayer(game));
-        
+
         assertThrows(RoundError.IllegalStateException.class, () -> gameService.getRound(GAME_ID, UNKNOWN_PLAYER_ID));
     }
-    
+
     @Test
     void testGetRound_GameNotFound() {
         mockGameNotFoundInRepository(GAME_ID);
-        
+
         assertThrows(GameError.NotFoundException.class, () -> gameService.getRound(GAME_ID, UNKNOWN_PLAYER_ID));
     }
-    
+
     @Test
     void testGetRound_ValidRound() {
         var game = mockGameInRepository();
@@ -132,7 +134,7 @@ class GameServiceTest {
         addActivePlayer(game, player2);
         addActivePlayer(game, player3);
         addActivePlayer(game, player4);
-        
+
         assertEquals(round, gameService.getRound(GAME_ID, player1.getId()));
         assertEquals(round, gameService.getRound(GAME_ID, player2.getId()));
         assertEquals(round, gameService.getRound(GAME_ID, player3.getId()));
@@ -141,39 +143,39 @@ class GameServiceTest {
 
     @Disabled
     @Test
-    void testSubmitProposition(){
-        Game game = mockGameInRepository();
-        Round round = getRound();
+    void testSubmitProposition() {
+        Game game = new Game(GAME_ID);
+        Round round = new Round("randomRound", PROMPT, 3, 3);
         game.addRound(round);
 
         gameService.submitProposition(round.getId(), UNKNOWN_PLAYER_ID, List.of("hello"));
 
         assertEquals(1, round.getPropositions().size());
     }
-    
+
     private Player addWaitingRoomPlayer(Game game) {
         Player player = new Player(UUID.randomUUID().toString());
         game.getWaitingRoom().put(player.getId(), player);
         return player;
     }
-    
+
     private Game mockGameInRepository() {
         var game = new Game(GAME_ID);
-        
+
         doNothing().when(gameEntityService).editGame(eq(game.getId()), any());
         when(gameEntityService.getGame(game.getId())).thenReturn(game);
-        
+
         return game;
     }
-    
+
     private void addActivePlayer(Game game, Player player) {
         game.getActivePlayers().put(player.getId(), player);
     }
-    
+
     private void mockGameNotFoundInRepository(String gameId) {
         doThrow(GameError.NotFoundException.class)
                 .when(gameEntityService).editGame(eq(gameId), any());
-        
+
         doThrow(GameError.NotFoundException.class)
                 .when(gameEntityService).getGame(gameId);
     }

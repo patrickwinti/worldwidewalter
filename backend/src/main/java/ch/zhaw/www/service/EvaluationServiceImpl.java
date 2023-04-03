16c14f1a3873fb92cb5c563a1f79fbfc1fb08434
Felix <opplifel@students.zhaw.ch>
2023-04-03 20:37:17 +0200

    implemented proposition to experiment

--- backend/src/main/java/ch/zhaw/www/service/EvaluationServiceImpl.java
+++ backend/src/main/java/ch/zhaw/www/service/EvaluationServiceImpl.java
@@ -1,20 +1,43 @@
 package ch.zhaw.www.service;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class EvaluationServiceImpl {
     Map<String, List<String>> selections = new HashMap<>();
+    List<Proposition> propositions = List.of(
+            new Proposition("ID-0001", "PlayerId_A", List.of("Oranges", "bananas"), false),
+            new Proposition("ID-0002", "PlayerId_B", List.of("Apples", "Oranges"), false),
+            new Proposition("ID-0003", "PlayerId_C", List.of("Yoghurt", "bananas"), false),
+            new Proposition("ID-0004", "PlayerId_D", List.of("Pancakes", "bananas"), false),
+            new Proposition("ID-0005", "PlayerId_E", List.of("Apples", "bananas"), true)
+    );
+
+    @Getter
+    @RequiredArgsConstructor
+    static class Proposition {
+        private final String proposition_Id;
+        private final String playerId;
+        private final List<String> gaps;
+        private final boolean isSphinxProposition;
+    }
 
     public void submitSelections() {
         // the ID is the key of the map and represents the proposition
         // the list of playerIDs is the value of the map and represents the players who selected the proposition
-        selections.put("ID-0001", List.of("PlayerID A", "PlayerID B", "PlayerID C"));
-        selections.put("ID-0002", List.of("PlayerID D", "PlayerID E"));
+        selections.put(propositions.get(0).proposition_Id, List.of(propositions.get(1).playerId, propositions.get(2).playerId));
+        selections.put(propositions.get(1).proposition_Id, List.of(propositions.get(2).playerId, propositions.get(3).playerId));
+        selections.put(propositions.get(2).proposition_Id, List.of(propositions.get(3).playerId, propositions.get(4).playerId));
+        selections.put(propositions.get(3).proposition_Id, List.of(propositions.get(4).playerId, propositions.get(0).playerId));
+
     }
 
-    public Map<String, Integer> evaluateRound() throws RoundError.IllegalStateException {
+    public Map<String, Integer> evaluateSelections() throws RoundError.IllegalStateException {
         Map<String, Integer> evaluationMap = new HashMap<>();
 
         // iterate over the entries in the selections map
@@ -42,7 +65,7 @@ public class EvaluationServiceImpl {
     public static void main(String[] args) {
         EvaluationServiceImpl evaluationService = new EvaluationServiceImpl();
         evaluationService.submitSelections();
-        Map<String, Integer> evaluationMap = evaluationService.evaluateRound();
+        Map<String, Integer> evaluationMap = evaluationService.evaluateSelections();
         evaluationService.printEvaluationMap(evaluationMap);
     }
 

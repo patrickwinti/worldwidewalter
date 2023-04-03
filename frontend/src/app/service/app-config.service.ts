10649bdf8af3f3b5ece176ab27a1c411b1aa0e1e
Patrick <schwipa2@students.zhaw.ch>
2023-04-03 23:52:15 +0200

    [WWW-88]
    call raspberry

--- frontend/src/app/service/app-config.service.ts
+++ frontend/src/app/service/app-config.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppConfigService {
-  private baseUrl: string = 'http://localhost:8080';
+  // private baseUrl: string = 'http://localhost:8080';
+  private baseUrl: string = 'http://myfilebox.synology.me:8080';
 
   getBaseUrl(): string {
     return this.baseUrl;

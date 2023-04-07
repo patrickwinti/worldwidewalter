fb717e6bd74004a23440c03ecf65a35e6e395613
Ricardo Duarte <duartric@students.zhaw.ch>
2023-04-07 10:24:21 +0200

    [WWW-99] Adding PromptService Class + Test

---
+++ backend/src/test/java/ch/zhaw/www/service/PromptServiceTest.java
@@ -0,0 +1,29 @@
+package ch.zhaw.www.service;
+
+import ch.zhaw.www.model.Prompt;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PromptServiceTest {
+
+
+    @Test
+    void shufflePrompts() {
+        List<Prompt> prompts1 = new ArrayList<>();
+        prompts1.add(new Prompt("Wenn du Raubkatzen fürchtest, so nähere dich nie einer Frau WALTER.\n", 1));
+        prompts1.add(new Prompt("Wenn meine Geliebte traurige Geschichten hören möchte, so erzähle ich ihr von der Zeit, als ich WALTERTE.", 1));
+        prompts1.add(new Prompt("Elektrische Installationen im Haushalt sind für Frauen WALTER.\n", 1));
+        prompts1.add(new Prompt("WALTER ist eine Schriftstellerin, die ich männlichen Koryphäen wie Goethe und Schiller bei weitem vorziehe.", 1));
+        prompts1.add(new Prompt("WALTER WALTERN WALTER", 3));
+
+        List<Prompt> prompts2 = List.copyOf(prompts1);
+
+        PromptService.shufflePrompts(prompts1);
+
+        assertNotEquals(prompts2, prompts1);
+    }
+}

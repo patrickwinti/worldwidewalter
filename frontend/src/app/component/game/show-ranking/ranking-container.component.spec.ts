69481ec782cb96684e36313632f9e6a241ccad57
Patrick <schwipa2@students.zhaw.ch>
2023-04-10 21:05:51 +0200

    [WWW-107] added test for sorting

--- frontend/src/app/component/game/show-ranking/ranking-container.component.spec.ts
+++ frontend/src/app/component/game/show-ranking/ranking-container.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RankingContainerComponent } from './ranking-container.component';
+import { ResultDto } from "../../../dto/results-dto";
 
 describe('ShowRankingComponent', () => {
   let component: RankingContainerComponent;
@@ -20,4 +21,26 @@ describe('ShowRankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should sort ranking', () => {
+    // arrange
+    var unsorted = [
+      {playerName: 'Hanna', points: 12} as ResultDto,
+      {playerName: 'Mudi', points: 3} as ResultDto,
+      {playerName: 'Herzog', points: 9} as ResultDto,
+      {playerName: 'Herzog', points: 1} as ResultDto
+    ];
+
+    // act
+    var sorted = component.sortRanking(unsorted);
+
+    // assert
+    expect(sorted).toEqual([
+      {playerName: 'Hanna', points: 12} as ResultDto,
+      {playerName: 'Herzog', points: 9} as ResultDto,
+      {playerName: 'Mudi', points: 3} as ResultDto,
+      {playerName: 'Herzog', points: 1} as ResultDto
+    ])
+  })
 });

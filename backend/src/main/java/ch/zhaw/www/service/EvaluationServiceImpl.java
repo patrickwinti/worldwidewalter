f738e963e55c331085f15a82ac3c092f7c6d1d5e
opplifel <opplifel@students.zhaw.ch>
2023-04-23 17:14:42 +0200

    Update backend/src/main/java/ch/zhaw/www/service/EvaluationServiceImpl.java
    Co-authored-by: vaquecar <vaquecar@students.zhaw.ch>

--- backend/src/main/java/ch/zhaw/www/service/EvaluationServiceImpl.java
+++ backend/src/main/java/ch/zhaw/www/service/EvaluationServiceImpl.java
@@ -25,7 +25,9 @@ public class EvaluationServiceImpl implements EvaluationService {
                 .ifPresent(proposition -> {
                     if (proposition.getPlayerIds().size() == 1) {
                         if (proposition.getPlayerIds().get(0).equals(sphinxId)) {
-                            evaluation.put(selectorId, SINGLE_POINT);
+                    if (proposition.getPlayerIds().size() > 1) {
+                        evaluation.put(selectorId, SINGLE_POINT);
+                    }
                             if (round.isAtLeastOneNoneSphinxPropositionHasBeenSelected()) {
                                 evaluation.put(sphinxId, SINGLE_POINT);
                             } else {

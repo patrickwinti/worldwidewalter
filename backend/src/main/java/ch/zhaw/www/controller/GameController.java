957b1d1e4f9330962f7d7c8070e085835f74c310
Patrick <schwipa2@students.zhaw.ch>
2023-04-23 22:41:40 +0200

    [WWW-120] - fixed import after merge

--- backend/src/main/java/ch/zhaw/www/controller/GameController.java
+++ backend/src/main/java/ch/zhaw/www/controller/GameController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;

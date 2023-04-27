81b2a2227e0ad7888faafebbd56f4c01c2ffd2e4
vaquecar <vaquecar@students.zhaw.ch>
2023-04-27 21:36:52 +0200

    [WWW-119] updates assets

---
+++ frontend/src/app/component/info/info.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'www-info',
+  templateUrl: './info.component.html'
+})
+export class InfoComponent {
+  collapsed: boolean = true;
+}

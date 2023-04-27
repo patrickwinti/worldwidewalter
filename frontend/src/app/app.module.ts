ee485118c8e634873d9ae8a6cc1baf62c1587f5d
vaquecar <vaquecar@students.zhaw.ch>
2023-04-27 21:48:26 +0200

    [WWW-119] formatting issue

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,29 +1,29 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {WelcomeComponent} from "./component/initialization/welcome/welcome.component";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {InitializationContainerComponent} from './component/initialization/initialization-container.component';
-import {JoinComponent} from './component/initialization/join/join.component';
-import {FormsModule} from "@angular/forms";
-import {GameContainerComponent} from './component/game/game-container.component';
-import {HttpPollingInterceptor} from "./service/http-polling.interceptor";
-import {ResultContainerComponent} from "./component/game/show-ranking/result-container.component";
-import {RoundComponent} from './component/game/round/round.component';
-import {OverlaySpinnerComponent} from './component/overlay-spinner/overlay-spinner.component';
-import {SphinxDisplayComponent} from './component/game/round/sphinx-display/sphinx-display.component';
-import {CountDownComponent} from './component/count-down/count-down.component';
-import {RankingDisplayComponent} from './component/game/show-ranking/ranking-display/ranking-display.component';
-import {EnterPropositionComponent} from "./component/game/round/enter-proposition/enter-proposition.component";
-import {SelectPropositionComponent} from "./component/game/round/select-proposition/select-proposition.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from "./component/initialization/welcome/welcome.component";
+import { HTTP_INTERCEPTORS , HttpClientModule} from "@angular/common/http";
+import { InitializationContainerComponent } from './component/initialization/initialization-container.component';
+import { JoinComponent } from './component/initialization/join/join.component';
+import { FormsModule } from "@angular/forms";
+import { GameContainerComponent } from './component/game/game-container.component';
+import { HttpPollingInterceptor } from "./service/http-polling.interceptor";
+import { ResultContainerComponent } from "./component/game/show-ranking/result-container.component";
+import { RoundComponent } from './component/game/round/round.component';
+import { OverlaySpinnerComponent } from './component/overlay-spinner/overlay-spinner.component';
+import { SphinxDisplayComponent } from './component/game/round/sphinx-display/sphinx-display.component';
+import { CountDownComponent } from './component/count-down/count-down.component';
+import { RankingDisplayComponent } from './component/game/show-ranking/ranking-display/ranking-display.component';
+import { EnterPropositionComponent } from "./component/game/round/enter-proposition/enter-proposition.component";
+import { SelectPropositionComponent } from "./component/game/round/select-proposition/select-proposition.component";
 import {
-  SelectionDisclosureComponent
+   SelectionDisclosureComponent
 } from './component/game/show-ranking/selection-disclosure/selection-disclosure.component';
-import {ErrorComponent} from './component/error/error.component';
-import {InfoComponent} from "./component/info/info.component";
-import {NgOptimizedImage} from "@angular/common";
+import { ErrorComponent } from './component/error/error.component';
+import { InfoComponent } from "./component/info/info.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @NgModule({
   declarations: [

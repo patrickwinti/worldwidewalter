fe897437c83bcee0bca790ac312cf3bb1c3b0d66
vaquecar <vaquecar@students.zhaw.ch>
2023-04-27 21:51:42 +0200

    [WWW-119] misguided whitespace

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from "./component/initialization/welcome/welcome.component";
-import { HTTP_INTERCEPTORS , HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { InitializationContainerComponent } from './component/initialization/initialization-container.component';
 import { JoinComponent } from './component/initialization/join/join.component';
 import { FormsModule } from "@angular/forms";

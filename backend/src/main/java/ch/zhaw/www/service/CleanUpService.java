b19341f5b579e259b3d6ae52a99514225729edd8
Patrick <schwipa2@students.zhaw.ch>
2023-05-07 12:40:21 +0200

    [WWW-167] - splitting run interval & max idle time - adding test

--- backend/src/main/java/ch/zhaw/www/service/CleanUpService.java
+++ backend/src/main/java/ch/zhaw/www/service/CleanUpService.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.stream.StreamSupport;
@@ -17,18 +16,16 @@ import static ch.zhaw.www.utils.InstantWrapper.isInFuture;
 public class CleanUpService {
     private final Logger logger = Logger.getLogger(CleanUpService.class.getSimpleName());
     
-    @Value("${cleanup.rate}")
-    private long cleanUpTimeInMinutes;
-    private final Duration cleanUpTimeDurationInMinutes;
+    @Value("${game.idle-time-before-removal}")
+    private Duration cleanUpTimeDurationInMinutes;
     private final GameRepository repository;
     
     public CleanUpService(GameRepository repository) {
         this.repository = repository;
-        this.cleanUpTimeDurationInMinutes = Duration.of(cleanUpTimeInMinutes, ChronoUnit.MINUTES);
     }
     
-    @Scheduled(fixedRateString = "${cleanup.rate}", timeUnit = TimeUnit.MINUTES)
-    private void runCleanUp() {
+    @Scheduled(fixedRateString = "${clean-up-service.game-cleanup.interval-in-min}", timeUnit = TimeUnit.MINUTES)
+    protected void runGameCleanUp() {
         logger.info("running game cleanup");
         synchronized (repository) {
             StreamSupport.stream(this.repository.findAll().spliterator(), false)

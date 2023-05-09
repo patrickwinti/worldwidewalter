a35f1603d2ecbbe65bcc99769c7d599f48e829f7
Patrick <schwipa2@students.zhaw.ch>
2023-05-09 08:53:52 +0200

    [WWW-198] - made variable static

--- backend/src/main/java/ch/zhaw/www/utils/RandomProviderImpl.java
+++ backend/src/main/java/ch/zhaw/www/utils/RandomProviderImpl.java
@@ -10,7 +10,7 @@ public class RandomProviderImpl implements RandomProvider {
     private final Random random = new Random(SEED);
     private static final int MIN_GAME_ID_LENGTH = 7;
 
-    private final char[] CHARS = new char[]{
+    private static final char[] CHARS = new char[]{
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
